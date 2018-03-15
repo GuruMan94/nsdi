8f383df69b9294eef55962698474dc162ef909dd
tsotne1994 <tsotne1994@gmail.com>
2018-03-15 14:24:51 +0400

    adding metamodel for contact

---
+++ src/generated/java/ge/tsotne/nsdi/model/Contact_.java
@@ -0,0 +1,24 @@
+package ge.tsotne.nsdi.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Contact.class)
+public abstract class Contact_ {
+
+	public static volatile SingularAttribute<Contact, String> firstName;
+	public static volatile SingularAttribute<Contact, String> lastName;
+	public static volatile SingularAttribute<Contact, String> phoneNumber;
+	public static volatile SingularAttribute<Contact, Boolean> active;
+	public static volatile SingularAttribute<Contact, Long> id;
+
+	public static final String FIRST_NAME = "firstName";
+	public static final String LAST_NAME = "lastName";
+	public static final String PHONE_NUMBER = "phoneNumber";
+	public static final String ACTIVE = "active";
+	public static final String ID = "id";
+
+}
+
