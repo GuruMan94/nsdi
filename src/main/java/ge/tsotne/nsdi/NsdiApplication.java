e2caf9d859731f535aca3f37b995c66af3cac3b3
tsotne1994 <tsotne1994@gmail.com>
2018-03-16 11:30:18 +0400

    some refactoring 2

--- src/main/java/ge/tsotne/nsdi/NsdiApplication.java
+++ src/main/java/ge/tsotne/nsdi/NsdiApplication.java
@@ -1,14 +1,10 @@
 package ge.tsotne.nsdi;
 
-import ge.tsotne.nsdi.repository.MyUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NsdiApplication{
-	@Autowired
-	MyUserRepository myUserRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(NsdiApplication.class, args);
 	}
